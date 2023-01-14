2e57959958907806adfc5979149b398e7564713f
ecclesias <77536370+zeviel@users.noreply.github.com>
2023-01-14 22:23:49 +0800

    Create hindi_quotes.py

---
+++ src/hindi_quotes.py
@@ -0,0 +1,17 @@
+from requests import get
+	
+class HindiQuotes:
+	def __init__(self) -> None:
+		self.api = "https://hindi-quotes.vercel.app"
+		self.headers = {
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
+		}
+
+	def get_random_quote(
+			self,
+			type: str = None) -> dict:
+		url = f"{self.api}/random"
+		if type:
+			url += "/{type}"
+		return get(
+			url, headers=self.headers).json()
